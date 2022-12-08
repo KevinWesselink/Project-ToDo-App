7b979443af17efe821859c6622a9e74785771ca4
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-12-08 13:44:05 +0100

    Database verbind succesvol met code.

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=todoapp", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -12,17 +12,3 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
-$dbname = "myDBPDO";
-
-try {
-    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-  VALUES ('John', 'Doe', 'john@example.com')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "New record created successfully";
-} catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-}
-
-$conn = null;
-?>
