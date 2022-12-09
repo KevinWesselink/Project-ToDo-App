1eb6530742431ab16604bc3627d0c6abe8aca381
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-12-09 10:18:48 +0100

    Input wordt opgeslagen in de database en kan er uit worden opgehaald.

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
+include "index.php";
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "todoapp";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=todoapp", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -12,3 +14,27 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
+//Data in de database stoppen
+$query = "insert into todoapp (`title`, `description`, `location`) values (:title, :description, :location)";
+echo $query;
+$sth = $conn->prepare($query);
+$sth->bindParam(':title', $_POST['title']);
+$sth->bindParam(':description', $_POST['description']);
+$sth->bindParam(':location', $_POST['location']);
+$sth->execute();
+
+//Data uit de database ophalen
+$query = "select id, title, description, location from todoapp";
+$sth = $conn->prepare($query);
+$sth->execute();
+//Geeft data weer in een associatieve array (data wordt niet opgehaald via indexes, maar via namen)
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+//Toon de data uit de array
+foreach($result as $row) {
+    echo $row['id'];
+    echo $row['title'];
+    echo $row['description'];
+    echo $row['location'];
+}
+?>
