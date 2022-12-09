c7cd494d82137760ada1e11cf002a8a3dfc32b25
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-12-09 12:14:09 +0100

    Comments toegevoegd bij verschillende functies.

--- db.php
+++ db.php
@@ -5,18 +5,19 @@ $username = "root";
 $password = "";
 $dbname = "todoapp";
 
+//Database connectie aangemaakt
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+//    echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 
 //Data in de database stoppen
 $query = "insert into todoapp (`title`, `description`, `location`) values (:title, :description, :location)";
-echo $query;
+//echo $query;
 $sth = $conn->prepare($query);
 $sth->bindParam(':title', $_POST['title']);
 $sth->bindParam(':description', $_POST['description']);
