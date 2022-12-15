1988b37f2282c487c60ccb442a663d2053cd2dff
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-12-15 12:24:55 +0100

    Dubbele data bug fix.

--- functions.php
+++ functions.php
@@ -10,12 +10,9 @@ if (isset($_POST['Opslaan'])) {
     $resultTitleDupeCheck = $conn->query("SELECT title FROM todoapp WHERE title = '$title'");
     $resultDescriptionDupeCheck = $conn->query("SELECT description FROM todoapp WHERE description = '$description'");
 
-//    echo $resultNameDupeCheck;
-//    echo $resultMsgDupeCheck;
-
     if ($resultTitleDupeCheck->rowCount() == 0 && $resultDescriptionDupeCheck->rowCount() == 0) {
 //Data in de database stoppen
-        $query = "insert into todoapp (`title`, `description`, `location`) values ($title, $description, $location)";
+        $query = "insert into todoapp (`title`, `description`, `location`) values (:title, :description, :location)";
 //echo $query;
         $sth = $conn->prepare($query);
         $sth->bindParam(':title', $title);
@@ -23,29 +20,31 @@ if (isset($_POST['Opslaan'])) {
         $sth->bindParam(':location', $location);
         $sth->execute();
     } else {
-        echo "Error: " . "<br>" . $conn->error;
+        echo "Error: duplicate entry";
     }
 }
 
-//Data uit de database ophalen
-$query = "SELECT id, title, description, location FROM todoapp";
-$sth = $conn->prepare($query);
-$sth->execute();
-//Geeft data weer in een associatieve array (data wordt niet opgehaald via indexes, maar via namen)
-$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+function getTodo()
+{
+    global $conn;
+    //Data uit de database ophalen
+    $query = "SELECT id, title, description, location FROM todoapp";
+    $sth = $conn->prepare($query);
+    $sth->execute();
+    //Geeft data weer in een associatieve array (data wordt niet opgehaald via indexes, maar via namen)
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-$createdOn = date("Y-m-d H:i:s");
+    $createdOn = date("Y-m-d H:i:s");
 
-//Toon de data uit de array
-foreach($result as $row) {
-    echo "<table class='tableBody'>
-            <tr>
-                <td>".$row['id']."</td>
-                <td>".$row['title']."</td>
-                <td>".$row['description']."</td>
-                <td>".$row['location']."</td>
-            </tr>
-        </table>";
+    //Toon de data uit de array
+    foreach ($result as $row) {
+        echo "<tr>
+                    <td>" . $row['id'] . "</td>
+                    <td>" . $row['title'] . "</td>
+                    <td>" . $row['description'] . "</td>
+                    <td>" . $row['location'] . "</td>
+                </tr>";
+    }
 }
 
 
