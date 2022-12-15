9d8efb362eb3e40eec5e4741a21324ca733b2201
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-12-15 13:39:42 +0100

    Css bestand versie gewijzigd.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include "functions.php"
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css?=v131220220915">
+    <link rel="stylesheet" href="css/style.css?=v151220221304">
     <title>To Do App</title>
 </head>
 <body>
