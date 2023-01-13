b185d85df28a4f999ec75da6fae9b040585456e2
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-01-13 12:21:15 +0100

    Style.css versie geüpdate.

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@ include "../src/functions.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css?=v120120231642">
+    <link rel="stylesheet" href="../css/style.css?=v130120231216">
     <title>To Do App</title>
 </head>
 <body>
